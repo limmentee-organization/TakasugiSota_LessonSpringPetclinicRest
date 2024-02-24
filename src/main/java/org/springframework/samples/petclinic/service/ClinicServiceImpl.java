ddb0081e452820216264bbc06b2d9ee16aac1e51
高杉宗汰 <sotatk25@KetsuzeiMacBook-Air.local>
2024-02-24 11:09:08 +0900

    vet_specialty完了

---
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
@@ -0,0 +1,109 @@
+package org.springframework.samples.petclinic.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.orm.ObjectRetrievalFailureException;
+import org.springframework.samples.petclinic.model.Specialty;
+import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.repository.SpecialtyRepository;
+import org.springframework.samples.petclinic.repository.VetRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClinicServiceImpl implements ClinicService {
+
+	private VetRepository vetRepository;
+	private SpecialtyRepository specialtyRepository;
+
+	@Autowired
+	public ClinicServiceImpl(VetRepository vetRepository, SpecialtyRepository specialtyRepository) {
+		this.vetRepository = vetRepository;
+		this.specialtyRepository = specialtyRepository;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Vet findVetById(int id) throws DataAccessException {
+		Vet vet = null;
+		try {
+			vet = vetRepository.findById(id);
+		} catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+			return null;
+		}
+		return vet;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Collection<Vet> findAllVets() throws DataAccessException {
+		return vetRepository.findAll();
+	}
+
+	@Override
+	@Transactional
+	public void saveVet(Vet vet) throws DataAccessException {
+		vetRepository.save(vet);
+	}
+
+	@Override
+	@Transactional
+	public void deleteVet(Vet vet) throws DataAccessException {
+		vetRepository.delete(vet);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Specialty findSpecialtyById(int specialtyId) {
+		Specialty specialty = null;
+		try {
+			specialty = specialtyRepository.findById(specialtyId);
+		} catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+			return null;
+		}
+		return specialty;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Collection<Specialty> findAllSpecialties() throws DataAccessException {
+		return specialtyRepository.findAll();
+	}
+
+	@Override
+	@Transactional
+	public void saveSpecialty(Specialty specialty) throws DataAccessException {
+		specialtyRepository.save(specialty);
+	}
+
+	@Override
+	@Transactional
+	public void deleteSpecialty(Specialty specialty) throws DataAccessException {
+		specialtyRepository.delete(specialty);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Specialty> findSpecialtiesByNameIn(Set<String> names) {
+		List<Specialty> specialties = new ArrayList<>();
+		try {
+			specialties = specialtyRepository.findSpecialtiesByNameIn(names);
+		} catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+			return specialties;
+		}
+		return specialties;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Collection<Vet> findVets() throws DataAccessException {
+		return vetRepository.findAll();
+	}
+
+}
